7543e607f857fbd8467c33c4b29b867981dd3226
MikeD789 <mldavisinc@gmail.com>
2025-01-26 00:02:14 -0500

    Updated  Lesson1_3Contents.swift

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
@@ -29,26 +29,71 @@ print(age)
 age += 1
 print("Updated age after birthday: \(age)")
 /*:
-var  
- */
-
-
-/*:
  Create a double variable with a value of 1.1. Update it to 2.2, 3.3, and 4.4, printing out the value after each assignment (again by referencing the variable you created).
  */
-
-
+var myDouble = 1.1
+print("Current value: \(myDouble)") 
+ myDouble = 2.2
+print("Updated value: \(myDouble)")  
+ myDouble = 3.3
+print("Updated value: \(myDouble)")  
+ myDouble = 4.4
+print("Updated value: \(myDouble)")  
 /*:
  Create a boolean variable and set it to `true`. Print the variable, then assign it a value of `false`, and print it again.
  */
-
-
+var isComplete = true
+print("Value: \(isComplete)")
+isComplete = false
+print("Updated value: \(isComplete)")
 /*:
  Create two variables, one with a value of 0, the other with a value of 0.0. Try to assign the second variable to the first, and you will receive an error. Add the necessary type annotation that will allow the second variable to be assigned to the first.
- */
+var integerVariable = 0
+var doubleVariable = 0.0
+//Attempting to assign the second variable to the first
+integerVariable = doubleVariable
+//Adding the nescessary type annotation 
+var integerVariable: Double = 0
+integerVariable = doubleVariable
+
+Create a variable integer with a value of 1,000,000,000, ensuring that you format it so it is more readable (i.e. it's hard to read 1000000000, so make it easier to read).
+ var largeNumber = 1_000_000_000
+ print(largeNumber)     
 
 
-/*:
- Create a variable integer with a value of 1,000,000,000, ensuring that you format it so it is more readable (i.e. it's hard to read 1000000000, so make it easier to read).
- */
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
