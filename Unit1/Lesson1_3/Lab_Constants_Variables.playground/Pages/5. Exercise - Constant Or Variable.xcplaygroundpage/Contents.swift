760db6b0e1972423adfc0e7eb493023132d65107
MikeD789 <mldavisinc@gmail.com>
2025-01-26 01:08:42 -0500

    Lesson1_3 exercise 5 complete

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
@@ -3,13 +3,23 @@
  
  Imagine you're creating a simple photo sharing app. You want to keep track of the following metrics for each post:
 - Number of likes: the number of likes that a photo has received
+var numberOfphotoLikes = 290
 - Number of comments: the number of comments other users have left on the photo
+var numberOfComments = 95
 - Year created: The year the post was created
+let yearCreated = 2025
 - Month created: The month the post was created represented by a number between 1 and 12
+let monthCreated = 1
 - Day created: The day of the month the post was created
- 
+ let dayCreated = 24
  For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
  */
+var numberOfphotoLikes = 290
+var numberOfphotoComments = 95
+let yearCreated = 2025
+let monthCreated = 1
+let dayCreated = 24
+
 
 
 /*:
