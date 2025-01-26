e58614625973c456366ce9ac537ae4eaa9cd2eff
MikeD789 <mldavisinc@gmail.com>
2025-01-26 15:18:41 -0500

    Lesson1_3 exercise 1 complete

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -9,11 +9,12 @@ print("Number of friends on social media: \(friends)")
 
 //:  Now assume you go through and remove friends that aren't active on social media. Attempt to update your `friends` constant to a lower number than it currently is. Observe what happens and then move to the next step.
 // Attempting to update my friends constant
+let friends = 475
 friends = 400
 
 
 //:  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
-print("The above code does not compile because friends was declared with the let keyword which makes it a constant and Swift language will not allow change once constants given or assigned a value")
+print("The above code does not compile because friends was declared with the let keyword which makes it a constant and Swift language will not allow change once a constant is given or assigned a value")
 
 
 /*:
