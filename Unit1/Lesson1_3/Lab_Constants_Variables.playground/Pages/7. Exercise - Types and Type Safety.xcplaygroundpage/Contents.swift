b11875ee2f8257889dd4d29b804f46afe8f1ab1f
MikeD789 <mldavisinc@gmail.com>
2025-01-26 16:08:19 -0500

    Lesson1_3 exercise 7 complete

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -10,16 +10,16 @@ var secondDecimal = 7.47
 
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 var trueOrFalse = true
-var firstDecimal: Double = trueOrFalse
+firstDecimal = trueOrFalse
 print("Error: Cannot assign a Boolean value to a variable of type Double")
-/ var firstDecimal: Double = trueOrFalse // This line should be removed removed
+// firstDecimal = trueOrFalse // This line should be removed 
 
 
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 var stringValue = "Hello, world!"
 var firstDecimal = 3.50
 
- firstDecimal = stringValue // This line is removed
+firstDecimal = stringValue // This line should be removed 
 
 print("Error: Cannot assign a String value to a variable of type Double")
 
@@ -27,7 +27,8 @@ print("Error: Cannot assign a String value to a variable of type Double")
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 var wholeNumber = 45
 var firstDecimal = 9.89
-firstDecimal = wholeNumber // This line is removed
+firstDecimal = wholeNumber // This line should be removed 
+
 print("Error: Cannot assign an Int value to a variable of type Double")
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
