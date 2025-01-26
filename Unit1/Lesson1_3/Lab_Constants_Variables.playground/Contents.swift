566028054763b2766bc1ac2aff136e23f372a0f5
MikeD789 <mldavisinc@gmail.com>
2025-01-25 21:07:44 -0500

    Started on Lesson 1_3

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
@@ -3,29 +3,33 @@
  
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
  */
-
-
+let friends = 475
+print("Number of friends on social media: \(friends)")
 /*:
  Now assume you go through and remove a lot of your friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is between 1 and 900.
  */
-
-
+// Attempting to update my friends constant
+friends = 400
+print("Number of friends updated on social media: \(friends)")
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
  */
-
+print("The above code does not compile because  friends was declared with the let keyword which makes it 
+      a constant and Swift language will not allow change once constants given or assigned a value")
 
 /*:
  Declare a variable `age` and set it to your own age. Print `age` to the console.
  */
-
-
+var age = 30
+print(age)
 /*:
  Now pretend you just had a birthday, and update the `age` variable accordingly. Print `age` to the console.
  */
-
+// Updating the variable for birthday
+age += 1
+print("Updated age after birthday: \(age)")
 /*:
- 
+var  
  */
 
 
