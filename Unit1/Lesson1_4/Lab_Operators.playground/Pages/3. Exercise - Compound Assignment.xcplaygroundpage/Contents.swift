6a07cb2fe83bfc0630bd9a11f4ba082ecfb7cf3b
MikeD789 <mldavisinc@gmail.com>
2025-01-26 03:48:04 -0500

    Lesson1_4 exercise 3 complete

--- Unit1/Lesson1_4/Lab_Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_4/Lab_Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
@@ -3,6 +3,16 @@
  
  Declare a variable whose value begins at 10. Using addition, update the value to 15 using the compound assignment operator. Using multiplication, update the value to 30 using compound assignment. Print out the variable's value after each assignment.
  */
+// Declare a variable with an initial value of 10
+var value = 10
+
+// Update the value to 15 using the compound assignment operator for addition
+value += 5
+print("The value after addition is \(value)")
+
+// Update the value to 30 using the compound assignment operator for multiplication
+value *= 2
+print("The value after multiplication is \(value)")
 
 
 /*:
@@ -16,6 +26,28 @@
  
  Print the balance of your penny bank after each step.
  */
+// Create a variable called pennyBank that begins at 0
+var pennyBank = 0
+
+// Your neighbor gives you 10 dollars for mowing her lawn
+pennyBank += 10
+print("After mowing the lawn, the balance is \(pennyBank) dollars")
+
+// You earn 20 more dollars throughout the week doing odd jobs
+pennyBank += 20
+print("After doing odd jobs, the balance is \(pennyBank) dollars")
+
+// You spend half your money on dinner and a movie
+pennyBank /= 2
+print("After spending on dinner and a movie, the balance is \(pennyBank) dollars")
+
+// You triple what's left in your penny bank by washing windows
+pennyBank *= 3
+print("After washing windows, the balance is \(pennyBank) dollars")
+
+// You spend 3 dollars at a convenience store
+pennyBank -= 3
+print("After spending at a convenience store, the balance is \(pennyBank) dollars")
 
 
 /*:
