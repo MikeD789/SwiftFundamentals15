d8fd791a21570e7d0bfd9f2c79da4b3735a25bad
MikeD789 <mldavisinc@gmail.com>
2025-02-16 18:59:47 -0500

    Lab 2-3_3 completed

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
@@ -3,12 +3,34 @@
  
  Write a new introduction function called `introduction`. It should take two `String` parameters, `name` and `home`, and one `Int` parameter, `age`. The function should print a brief introduction. I.e. if "Jenny," "California," and 32 were passed into the function, it might print "Jenny, 32, is from California." Call the function and observe the printout.
  */
+func introduction(name: String, home: String, age: Int) {
+    print("\(name), \(age), is from \(home).")
+}
+
+// Call the function 
+introduction(name: "Jenny", home: "California", age: 32)
 
 
 //:  Write a function called `almostAddition` that takes two `Int` arguments. The first argument should not require an argument label. The function should add the two arguments together, subtract 2, then print the result. Call the function and observe the printout.
+func almostAddition(_ firstNumber: Int, secondNumber: Int) {
+    let result = firstNumber + secondNumber - 2
+    print(result)
+}
+
+// Call the function and observe the printout
+almostAddition(5, secondNumber: 3)  
+almostAddition(10, secondNumber: 7) 
 
 
 //:  Write a function called `multiply` that takes two `Double` arguments. The function should multiply the two arguments and print the result. The first argument should not require a label, and the second argument should have an external label, `by`, that differs from the internal label. Call the function and observe the printout.
+func multiply(_ firstNumber: Double, by secondNumber: Double) {
+    let result = firstNumber * secondNumber
+    print(result)
+}
+
+// Call the function and observe the printout
+multiply(2.7, by: 3.5) // Output: 9.45
+multiply(8.3, by: 4.1) // Output: 34.03
 
 
 /*:
