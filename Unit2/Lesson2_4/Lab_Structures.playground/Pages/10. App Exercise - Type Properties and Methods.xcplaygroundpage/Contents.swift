fe288c87770cf324df669842c47cd5a248646122
MikeD789 <mldavisinc@gmail.com>
2025-02-16 20:58:54 -0500

    Lab 2-4_10 completed

--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/10. App Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/10. App Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -13,11 +13,26 @@ struct RunningWorkout {
     var distance: Double
     var time: Double
     var elevation: Double
+    
+    static let meterInFeet = 3.28084
+    static let mileInMeters = 1600.0
+    
+    static func mileTimeFor(distance: Double, time: Double) -> Double {
+        return time / (distance / mileInMeters)
+    }
 }
+
+// Call the method from outside of the struct and print the result
+let averageMileTime = RunningWorkout.mileTimeFor(distance: 3200, time: 30)
+print("Average Mile Time: \(averageMileTime) minutes")
+
+
 
 
 
 //:  It may be helpful to have a few type properties on `RunningWorkout` representing unit conversions (i.e. meters to mile, feet to meters, etc.). Go back and add a type property for `meterInFeet` and assign it 3.28084. Then add a type property for `mileInMeters` and assign it 1600.0. Print both of these values below.
+print("Meters in a Mile: \(RunningWorkout.mileInMeters)")
+print("Meters in a Foot: \(RunningWorkout.meterInFeet)")
 
 
 /*:
