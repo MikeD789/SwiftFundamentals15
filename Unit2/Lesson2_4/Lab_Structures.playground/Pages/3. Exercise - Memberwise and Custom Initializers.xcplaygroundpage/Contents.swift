358b55708e8f0adc9f8734044ad35a71edc40e5e
MikeD789 <mldavisinc@gmail.com>
2025-02-16 19:54:21 -0500

    Lab 2-4_3 completed

--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/3. Exercise - Memberwise and Custom Initializers.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/3. Exercise - Memberwise and Custom Initializers.xcplaygroundpage/Contents.swift
@@ -5,17 +5,47 @@
  */
 struct GPS {
 
-
+    var latitude: Double
+    var longitude: Double
 }
+
+
+
 //:  Now create a constant instance of `GPS` called `somePlace`, and use the memberwise initializer to set `latitude` to 51.514004, and `longitude` to 0.125226. Print the values of `somePlace`'s properties.
+let somePlace = GPS(latitude: 51.514004, longitude: 0.125226)
+print("Latitude: \(somePlace.latitude), Longitude: \(somePlace.longitude)")
 
 
 //:  In Structs, Instance, and Default Values, you also created a `Book` struct with properties `title`, `author`, `pages`, and `price`. Create this struct again without default values. Give each property the appropriate type. Declare your `favoriteBook` instance and pass in the values of your favorite book using the memberwise initializer. Print a statement about your favorite book using `favoriteBook`'s properties.
+struct Book {
+    var title: String
+    var author: String
+    var pages: Int
+    var price: Double
+}
+let favoriteBook = Book(title: "Mosaic", author: "John R. Maxim", pages: 448, price: 5.15)
+
+print("Title: \(favoriteBook.title)")
+print("Author: \(favoriteBook.author)")
+print("Pages: \(favoriteBook.pages)")
+print("Price: $\(favoriteBook.price)")
 
 
 /*:
  Make a `Laptop` struct with three variable properties, `screenSize` of type `Int`, `repairCount` of type `Int`, and `yearPurchased` of type `Int`. Give `screenSize` a default value of `13` and `repairCount` a default value of `0`, and leave `yearPurchased` without a default value. Declare two instances of `Laptop`, using the two provided memberwise initializers.
  */
+struct Laptop {
+    var screenSize: Int = 13
+    var repairCount: Int = 0
+    var yearPurchased: Int
+}
+
+// Declare two instances using memberwise initializers:
+let firstLaptop = Laptop(yearPurchased: 2020)
+let secondLaptop = Laptop(screenSize: 15, repairCount: 2, yearPurchased: 2021)
+
+print("First Laptop - Screen Size: \(firstLaptop.screenSize), Repair Count: \(firstLaptop.repairCount), Year Purchased: \(firstLaptop.yearPurchased)")
+print("Second Laptop - Screen Size: \(secondLaptop.screenSize), Repair Count: \(secondLaptop.repairCount), Year Purchased: \(secondLaptop.yearPurchased)")
 
 
 /*:
@@ -24,13 +54,58 @@ struct GPS {
  Create two custom initializers. One initializer will take a `Double` argument that represents height in inches. The other initializer will take a `Double` argument that represents height in centimeters. Each initializer should take the passed in value and use it to set the property that corresponds to the unit of measurement passed in. It should then set the other property by calculating the right value from the passed in value. Hint: *1 inch = 2.54 centimeters*.
 
  - Example: If you use the initializer for inches to pass in a height of 65, the initializer should set `heightInInches` to 65 and `heightInCentimeters` to 165.1.
- */
 
+*/
+import Foundation
+
+struct Height {
+    var heightInInches: Double
+    var heightInCentimeters: Double
+    
+    init(heightInInches: Double) {
+        self.heightInInches = heightInInches
+        self.heightInCentimeters = heightInInches * 2.54
+    }
+    
+    init(heightInCentimeters: Double) {
+        self.heightInCentimeters = heightInCentimeters
+        self.heightInInches = heightInCentimeters / 2.54
+    }
+}
 
 //:  Now create a variable instance of `Height` called `someonesHeight`. Use the initializer for inches to set the height to 65. Print out the property for height in centimeters and verify that it is equal to 165.1.
+// Create a variable instance of `Height` called `someonesHeight`
+var someonesHeight = Height(heightInInches: 65)
 
+print("Height in inches: \(someonesHeight.heightInInches)")
+print("Height in centimeters: \(someonesHeight.heightInCentimeters)")
+
+
+assert(someonesHeight.heightInCentimeters == 165.1, "The height in centimeters should be 165.1")
 
 //:  Now create a variable instance of `Height` called `myHeight` and initialize it with your own height. Verify that both `heightInInches` and `heightInCentimeters` are accurate.
+struct Height {
+    var heightInInches: Double
+    var heightInCentimeters: Double
+    
+    init(heightInInches: Double) {
+        self.heightInInches = heightInInches
+        self.heightInCentimeters = heightInInches * 2.54
+    }
+    
+    init(heightInCentimeters: Double) {
+        self.heightInCentimeters = heightInCentimeters
+        self.heightInInches = heightInCentimeters / 2.54
+    }
+}
+
+// Create a variable instance of `Height` called `someonesHeight`
+var someonesHeight = Height(heightInInches: 65)
+print("Height in centimeters: \(someonesHeight.heightInCentimeters)")
+
+// Create a variable instance of `Height` called `myHeight`
+var myHeight = Height(heightInInches: 70) 
+print("My height in inches: \(myHeight.heightInInches), My height in centimeters: \(myHeight.heightInCentimeters)")
 
 
 /*:
