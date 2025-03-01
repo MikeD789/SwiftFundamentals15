dfeceea63ce62496f4080608a59c9eac32d51030
MikeD789 <mldavisinc@gmail.com>
2025-03-01 18:53:25 -0500

    2_9 complete

---
+++ Unit2/Lesson2_9/Lab: Tutorial Screen/Lab: Tutorial ScreenTests/Lab__Tutorial_ScreenTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab__Tutorial_ScreenTests.swift
+//  Lab: Tutorial ScreenTests
+//
+//  Created by spring2025 on 3/1/25.
+//
+
+import Testing
+@testable import Lab__Tutorial_Screen
+
+struct Lab__Tutorial_ScreenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
